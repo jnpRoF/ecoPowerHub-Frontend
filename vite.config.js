703daefb93e61095458e21dcff9c88c3942227cc
Johnson <ogbonnayajprof2015@gmail.com>
2023-11-24 07:42:27 +0100

    restore vite config file

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,20 @@
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// export default defineConfig(({ mode }) => {
+//     const env = loadEnv(mode, process.cwd(), '')
+//     return {
+//         define: {
+//             'process.env': env,
+//         },
+//         plugins: [react()],
+//     }
+// })
+
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd(), '')
-    return {
-        define: {
-            'process.env': env,
-        },
-        plugins: [react()],
-    }
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [react()],
 })
